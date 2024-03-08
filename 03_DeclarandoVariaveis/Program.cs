c45d597baacb65e9fe636fadcb536838f4f84a19
agnoslima <agnos.nl@gmail.com>
2024-03-08 15:54:51 -0300

    Declarando variaveis

---
+++ 03_DeclarandoVariaveis/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace DeclarandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // tipo e nome
+            // posso declarar assim que declaro a variavel
+            int idade;
+            decimal altura;
+            string nome;
+
+            nome = "Agnos";
+            altura = 1.70m;
+            idade = 31;
+            Console.WriteLine("Olá " +nome);
+            Console.WriteLine("Você tem, " +idade);
+            Console.WriteLine("e tem, " +altura);
+        }
+    }
+}
+
+
