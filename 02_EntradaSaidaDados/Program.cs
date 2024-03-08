69e3cae2e001f4f1d9a5d3ef2a556affbfacdf36
agnoslima <agnos.nl@gmail.com>
2024-03-08 14:22:28 -0300

    entrada e saida de dados

---
+++ 02_EntradaSaidaDados/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace EntradaSaidaDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o seu nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("bem vindo (a) " +nome);
+    }
+    }
+}
+
