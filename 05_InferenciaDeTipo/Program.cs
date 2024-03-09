35ef8de178daa3a86c8d09b4d7fb28ebf48425ec
agnoslima <agnos.nl@gmail.com>
2024-03-09 11:07:30 -0300

    Trabalhando com inferencia de dados

---
+++ 05_InferenciaDeTipo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InferenciaDeTipos
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // exemplo de inferencia de tipo
+            // dever ser inicalizadas com valor
+            // o valor atribuido nao pode ser nulo
+            var nome = "Agnos";
+            Console.WriteLine(nome);
+        }
+    }
+}
