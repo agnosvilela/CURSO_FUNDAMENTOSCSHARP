b65c916181942613402ec1e4fad24ddfaca89e85
agnoslima <agnos.nl@gmail.com>
2024-03-09 11:09:05 -0300

    Trabalhando tipos de dados em c#

---
+++ 04_TiposDeDados/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Tipos de dados em c# é de extrema importancia.
+ No c# seja da declaração de uma variavel ou por inferencia do compilador é necessário 
+atribuir qual tipo terar determinado valor de variável.
+Number
+int 32 bits 32 /8 = bytes
+float 4 bytes 4 * 8 = bits
+double 8 bytes  8 * 8 = bits
+decimal 16 bytes 16 * 8 = bits
+
+string
+char 2bytes
+string 2bits por caractere
+
+boolean
+bool 1 bit
+
+
+ */
